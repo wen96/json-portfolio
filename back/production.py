c0258d63e3997077b89528c0e491e3d20e430b40
Rubén Pardo <yosoyruben@gmail.com>
2018-01-11 23:42:09 +0100

    Removing zip manifest

--- back/production.py
+++ back/production.py
@@ -3,11 +3,9 @@ import dj_database_url
 from back.settings import *
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
