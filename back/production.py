7c7df4b98536a5d249d183a18ac5db31efbbf7ae
Rubén Pardo <yosoyruben@gmail.com>
2018-01-11 23:19:07 +0100

    Trying to fix collectstatic on heroku

--- back/production.py
+++ back/production.py
@@ -2,6 +2,19 @@ import os
 import dj_database_url
 from back.settings import *
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DEBUG = False
