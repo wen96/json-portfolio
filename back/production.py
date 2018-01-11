1ac375228276d87ee4c969852665058123129da8
Rubén Pardo <yosoyruben@gmail.com>
2018-01-11 23:40:10 +0100

    Removing zip manifest

--- back/production.py
+++ back/production.py
@@ -15,7 +15,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DEBUG = False
 
